490642223feebadd6e2327967e2b457bf91d70d2
rijun <rijun@INrijun.local>
2015-08-27 23:17:17 +0530

    Should equate two same points

--- test/com/thoughtworks/testline/LineTest.java
+++ test/com/thoughtworks/testline/LineTest.java
@@ -10,36 +10,49 @@ public class LineTest {
     @Test
     public void shouldCalculateLengthOfALineAsObjectWithCoordinateProperties() {
         Line line = new Line(new Point(0, 0), new Point(1, 1));
+
         assertEquals(1.41421, line.calculateLength(), 0.0001);
     }
 
     @Test
     public void shouldNotEquateLineWithNullObject() {
         Line line = new Line(new Point(0, 0), new Point(0, 0));
+
         assertNotEquals(line, null);
     }
 
     @Test
     public void shouldNotEquateWithAnObjectOfDifferentType() {
         Line line = new Line(new Point(0, 0), new Point(0, 0));
+
         assertNotEquals(line, "String");
     }
 
     @Test
     public void shouldSuccessfullyEnforceThatLineUsesPointObjectsAsFields() {
         Line line = new Line(new Point(0, 0), new Point(0, 0));
+
         assertEquals(line, line);
     }
 
     @Test
     public void shouldEquatePointToItself() {
         Point point = new Point(0, 0);
+
         assertEquals(point, point);
     }
 
     @Test
     public void shouldNotEquatePointToNullObject() {
         Point point = new Point(0, 0);
+    }
+
+    @Test
+    public void shouldEquateTwoSamePoints() {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
+
+        assertEquals(point1, point2);
     }
 
 }
