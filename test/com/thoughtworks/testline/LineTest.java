7a755d78b5090a82ee1baf6c8bfc4036d56a0edc
rijun <rijun@INrijun.local>
2015-08-27 21:47:03 +0530

    Check if a line equates with itself

--- test/com/thoughtworks/testline/LineTest.java
+++ test/com/thoughtworks/testline/LineTest.java
@@ -13,4 +13,10 @@ public class LineTest {
         assertEquals(1.41421, line.calculateLength(), 0.0001);
     }
 
+    @Test
+    public void shouldEquateLineWithItself() {
+        Line line = new Line(0, 0, 0, 0);
+        assertEquals(line, line);
+    }
+
 }
